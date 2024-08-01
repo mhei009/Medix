8d05b15a2327aaba870d4748c2932a77aa0c9b40
Heidi <emhp009@gmail.com>
2024-08-01 11:33:48 +0200

    customfpormfield edit

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -5,10 +5,19 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import CustomFormField from "../ui/CustomFormField";
 
-const formSchema = z.object({
+export enum FormFieldType {
+  INPUT = "input",
+  TEXTAREA = "textarea",
+  PHONE_INPUT = "phoneInput",
+  CHECKBOX = "checkbox",
+  DATE_PICKER = "datePicker",
+  SELECT = "select",
+  SKELETON = "skeleton",
+}
+
+export const formSchema = z.object({
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
   }),
@@ -38,7 +47,15 @@ const PatientForm = () => {
           </p>
         </section>
 
-        <CustomFormField control={form.control} />
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="name"
+          label="Full name"
+          placeholder="John Doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
 
         <Button type="submit">Submit</Button>
       </form>
--- components/ui/CustomFormField.tsx
+++ components/ui/CustomFormField.tsx
@@ -9,24 +9,34 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { any } from "zod";
+import { FormFieldType } from "../forms/PatientForm";
+import { Control } from "react-hook-form";
 
 interface CustomProps {
   control: Control<any>;
+  fieldType: FormFieldType;
+  name: string;
+  label?: string;
+  placeholder?: string;
+  iconSrc?: string;
+  stringiconALt?: string;
+  disabled?: boolean;
+  dateFormat?: string;
+  showTimeSelect?: boolean;
+  children?: React.ReactNode;
+  renderSkeleton?: (field: any) => React.ReactNode;
 }
 
-const CustomFormField = ({ control }: CustomProps) => {
+const CustomFormField = ({ control, fieldType, name, label }: CustomProps) => {
   return (
     <FormField
       control={control}
-      name="username"
+      name={name}
       render={({ field }) => (
-        <FormItem>
-          <FormLabel>Username</FormLabel>
-          <FormControl>
-            <Input placeholder="username / email" {...field} />
-          </FormControl>
-          <FormDescription>This is your public display name.</FormDescription>
-          <FormMessage />
+        <FormItem className="flex-1">
+          {fieldType !== FormFieldType.CHECKBOX && label && (
+            <FormLabel>{label}</FormLabel>
+          )}
         </FormItem>
       )}
     />
