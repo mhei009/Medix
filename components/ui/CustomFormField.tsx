56752251f7c0a6500f54c0aa847499866e543293
Heidi <emhp009@gmail.com>
2024-08-01 12:02:51 +0200

    fields and props added in form

--- components/ui/CustomFormField.tsx
+++ components/ui/CustomFormField.tsx
@@ -27,7 +27,12 @@ interface CustomProps {
   renderSkeleton?: (field: any) => React.ReactNode;
 }
 
-const CustomFormField = ({ control, fieldType, name, label }: CustomProps) => {
+const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
+  return <Input type="text" placeholder="John Doe" />;
+};
+
+const CustomFormField = (props: CustomProps) => {
+  const { control, fieldType, name, label } = props;
   return (
     <FormField
       control={control}
@@ -37,6 +42,10 @@ const CustomFormField = ({ control, fieldType, name, label }: CustomProps) => {
           {fieldType !== FormFieldType.CHECKBOX && label && (
             <FormLabel>{label}</FormLabel>
           )}
+
+          <RenderField field={field} props={props} />
+
+          <FormMessage className="shad-error" />
         </FormItem>
       )}
     />
