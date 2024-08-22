c06fabe2c241373536e7771ebfb2f6d482b7079c
Heidi <emhp009@gmail.com>
2024-08-23 00:32:52 +0200

    admin page on process

---
+++ app/admin/page.tsx
@@ -0,0 +1,61 @@
+
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import StatCard from '@/components/ui/StatCard'
+import { getRecentAppointmentList } from '@/lib/actions/appointment.actions'
+
+const Admin = async () => {
+const appointments = await getRecentAppointmentList();
+
+  return ( 
+    <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
+        <header className='admin-header'>
+            <Link href='/' className='cursor-pointer'>
+                <Image
+                    src='/assets/icons/logo-full.svg'
+                    alt='Logo'
+                    width={132}
+                    height={162}
+                    className='h-8 w-fit'
+                    />
+            </Link>
+            <p className='text-16-semibold'>Admin Dashboard</p>
+        </header>
+
+        <main className='admin-main'>
+            <section className='w-full space-y-4'>
+                <h1 className='header'>Welcome, Admin</h1>
+                <p className='text-dark-700'>Manage new appointments.</p>
+            </section>
+
+            <section className='admin-stat'>
+            
+            <StatCard
+            type="appointments"
+            count={appointments.scheduledCount}
+            label="Scheduled appointments"
+            icon={"/assets/icons/appointments.svg"}
+          />
+                
+                <StatCard 
+                    type="pending"
+                    count={appointments.pendingCount}
+                    label="Pending appointments"
+                    icon="/assets/icons/pending.svg"
+                />
+                
+                <StatCard 
+                    type="cancelled"
+                    count={appointments.cancelledCounts}
+                    label="Cancelled appointments"
+                    icon="/assets/icons/cancelled.svg"
+            />
+            </section>
+
+        </main>
+    </div>
+  )
+}
+
+export default Admin
--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,8 +1,10 @@
 'use server'
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases, PATIENT_COLLECTION_ID } from "../appwrite.config";
 import { parseStringify } from "../utils";
+import { Appointment } from "@/types/appwrite.types";
+
 
 export const createAppointment = async (appointment: CreateAppointmentParams) => {
     try {
@@ -33,4 +35,55 @@ export const getAppointment = async (appointmentId: string) => {
         console.log(error)
     }
 }
+   
+export const getRecentAppointmentList = async () => {
+    try {
+      const appointments = await databases.listDocuments(
+        DATABASE_ID!,
+        APPOINTMENT_COLLECTION_ID!,
+        [Query.orderDesc('$createdAt')]
+      );
+  
+      
+      const initialCounts = {
+        scheduledCount: 0,
+        pendingCount: 0,
+        cancelledCount: 0,
+      };
+  
     
+      const counts = (appointments.documents as Appointment[]).reduce(
+        (acc, appointment) => {
+          switch (appointment.status) {
+            case 'scheduled':
+              acc.scheduledCount += 1;
+              break;
+            case 'pending':
+              acc.pendingCount += 1;
+              break;
+            case 'cancelled':
+              acc.cancelledCount += 1;
+              break;
+            default:
+              break; 
+          }
+          return acc;
+        },
+        initialCounts
+      );
+  
+
+      const data = {
+        totalCount: appointments.total,
+        ...counts,
+        documents: appointments.documents 
+      };
+  
+      return parseStringify(data);
+  
+    } catch (error) {
+      console.error('Error fetching appointments:', error);
+      
+ 
+    }
+  };
