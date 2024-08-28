a2583089bd229a62844717c9cb5a8919ee591d7b
Heidi <emhp009@gmail.com>
2024-08-28 12:21:41 +0200

    create log in

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Home({searchParams}: SearchParamProps) {
         </div>
       </section>
       <Image
-        src="/assets/images/onboarding-img.png"
+        src="/assets/images/onboarding-img.jpg"
         height={1000}
         width={1000}
         alt="onboardingdoctor"
