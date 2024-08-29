8c80e8472969720a7feb11f8dde029aaa4249360
Heidi <emhp009@gmail.com>
2024-08-29 19:17:11 +0200

    admin still not working

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,8 +1,13 @@
 
+
+import PatientForm from "@/components/forms/PatientForm";
+import { Button } from "@/components/ui/button";
 import { getPatient } from "@/lib/actions/patient.actions";
 import Image from "next/image";
 import *  as Sentry from '@sentry/nextjs'
 import { AppointmentForm } from "@/components/forms/AppointmentForm";
+
+
 
 
 export default async function NewAppointment({ params: { userId } }: SearchParamProps) {
